2c220a448e41b1e6ca747e26a7041bbe70daf3d0
Muhammad Shoaib Tahir <shoaibtahir410@gmail.com>
2025-07-11 17:45:46 +0500

    Initial commit with updated backend and frontend

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import spacy
 import textstat
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -85,5 +85,6 @@ def analyze():
 
     return jsonify({"tokens": results, "extra": extra})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
