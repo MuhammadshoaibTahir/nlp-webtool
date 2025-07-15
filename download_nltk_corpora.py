bcb46fc4f278be4c53e0b9635e030ad6e81456d8
Muhammad Shoaib Tahir <shoaibtahir410@gmail.com>
2025-07-16 01:51:22 +0500

    Add nltk_data and fix TextBlob error

---
+++ download_nltk_corpora.py
@@ -0,0 +1,5 @@
+import nltk
+nltk.download('brown', download_dir='nltk_data')
+nltk.download('punkt', download_dir='nltk_data')
+nltk.download('wordnet', download_dir='nltk_data')
+nltk.download('averaged_perceptron_tagger', download_dir='nltk_data')
